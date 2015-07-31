bfc3faa3739acbb666e1e39bb35339fff4d9de4d
glpaparelli <giuliopaparelli@gmail.com>
2015-07-31 18:51:25 +0200

    bug relativi accesso e iscrizione

--- public_html/controllers/loginController.js
+++ public_html/controllers/loginController.js
@@ -3,5 +3,25 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/* global angular */
+
+angular.module("urlSubscribed")
+.constant("userLoginUrl", "http://localhost:5500/abbonato/login")
+.controller("loginCtrl", function($scope, $http, userLoginUrl, $location){
+    
+    $scope.logIn = function(){
+        $http.post(userLoginUrl, {
+            username: $scope.login.username,
+            password: $scope.login.password
+        }, {
+            withCredentials: true
+        }).success(function (data) {
+            $location.path("/subscribed.html");
+        }).error(function (error) {
+            $scope.error = error;
+        });
+    }; 
+   
+});
 
 
--- public_html/controllers/subscribeController.js
+++ public_html/controllers/subscribeController.js
@@ -4,4 +4,30 @@
  * and open the template in the editor.
  */
 
-
+angular.module("urlSubscribed")
+.constant("userUrl", "http://localhost:5500/abbonato/login")
+.controller("subscribeCtrl", function($scope, $http, userUrl, $location){
+     
+    $scope.subscription = function(){
+        console.log("ehi!");
+        var sub = {
+            username:$scope.subscribe.username,
+            password:$scope.subscribe.password,
+            age:$scope.subscribe.age,
+            mail: $scope.subscribe.mail,
+            weight:$scope.subscribe.weight,
+            height:$scope.subscribe.height
+        };
+                
+        $http.post(userUrl, sub).
+        success(function(data) {
+            console.log("Si");
+            $location.path("/subscribed.html");
+        }).
+        error(function(error) {
+            $scope.error = error;
+        });
+    };
+    
+    
+});
