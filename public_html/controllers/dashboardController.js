42e252f7e5a963b19a1b079a50d8e0792383177f
Lorenzo <lrgazzella@gmail.com>
2015-08-01 22:26:40 +0200

    Modificata dashboard

--- public_html/controllers/dashboardController.js
+++ public_html/controllers/dashboardController.js
@@ -1,7 +1,14 @@
 angular.module("adminModule")
 .constant("userUrl", "http://localhost:5500/abbonato")
 .constant("boardUrl", "http://localhost:5500/scheda")
-.controller("adminCtrl", function($scope, $http, $location, userUrl, boardUrl){
+.controller("adminCtrl", function($scope, $http, $location, userUrl, boardUrl, $rootScope){
+    
+    console.log($location.path());
+    if($location.path() == "/dashboard"){
+        console.log("SI");
+    }
+    
+    $rootScope.showSettings = true;
     
     $scope.userSelected;
        
@@ -23,28 +30,48 @@ angular.module("adminModule")
         $scope.userSelected = user;
     };
     
-    $scope.deleteUser = function(user){
+    $scope.deleteUser = function(user){//da riguardare
         var url = userUrl + "/" + user.id;
         var index;
-        angular.forEach($scope.subscribers.data, function(value, key){
+        angular.forEach($scope.subscribers, function(value, key){
             if(user == value){
                 index = key;
             }
         });
+        
         $http.delete(url, user)
         .success(function(){
-            $scope.users.splice(index, 1);
+            $scope.subscribers.splice(index, 1);
         }).error(function(error){
             $scope.subscribers.errorDetele = error;
         });
     };
     
-    $scope.showButton = function(button){
+    $scope.showButton = function(){
         $scope.button = true;
     };
     
     $scope.selectDuration = function(duration){
         $scope.durationSelected = duration;
     };
+    
+    $scope.uniqueUsername = function(username){
+        var exit = false;
+        for(var i=0; ((i<$scope.subscribers.length) && (exit==false));i++){
+            if(username == $scope.subscribers[i].username){
+                $scope.button = true;
+                $scope.showMessage = true;
+                exit = true;
+            }else{
+                $scope.button = false;
+                $scope.showMessage = false;
+            }
+        }
+    };
+    
+    $scope.logout = function(){
+        console.log("Logout");
+    };
+    
 });
 
